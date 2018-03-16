27295a4aef302f19a46890ffd6f38099e6cbd7f2
Myroslav Kolodii <mkolodii@gmail.com>
2018-03-16 14:52:10 +0100

    Connect to Realtime Database.

--- app/src/main/java/com/unagit/deskmanagementtool/MainActivity.java
+++ app/src/main/java/com/unagit/deskmanagementtool/MainActivity.java
@@ -1,13 +1,41 @@
 package com.unagit.deskmanagementtool;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.firestore.DocumentSnapshot;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        // Access a Cloud Firestore instance from your Activity
+//        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//
+//        db.collection("persons")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (DocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
+//
     }
 }
