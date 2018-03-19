1e8f439fd6d5cd3d3fb43f6a8210c1f9978b0132
Myroslav Kolodii <mkolodii@gmail.com>
2018-03-19 22:40:37 +0100

    Implement date pickers.

--- app/src/main/java/com/unagit/deskmanagementtool/activities/AddAbsenceActivity.java
+++ app/src/main/java/com/unagit/deskmanagementtool/activities/AddAbsenceActivity.java
@@ -1,8 +1,16 @@
 package com.unagit.deskmanagementtool.activities;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.unagit.deskmanagementtool.R;
 
@@ -11,7 +19,19 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class AddAbsenceActivity extends AppCompatActivity {
+public class AddAbsenceActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    private final static String START_DATE_TAG = "startDatePicker";
+    private final static String END_DATE_TAG = "endDatePicker";
+    private static SimpleDateFormat format;
+    android.support.v4.app.DialogFragment fragment;
+    private static Date startDate;
+    private static Date endDate;
+
+    private enum DateEditText {
+        startDateEditText,
+        endDateEditText
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +46,128 @@ public class AddAbsenceActivity extends AppCompatActivity {
     }
 
     private void initializeDatePickers() {
-        EditText startDateEditText = findViewById(R.id.start_date_editText);
-        EditText endDateEditText = findViewById(R.id.end_date_editText);
-
-
-        SimpleDateFormat format = new SimpleDateFormat("EEE, MMMM dd, yyyy", Locale.getDefault());
+        // Add current date to both pickers.
+        format = new SimpleDateFormat("EEE, MMMM dd, yyyy", Locale.getDefault());
         String now = format.format(new Date());
+        setDateInEditText(DateEditText.startDateEditText, now, new Date());
+        setDateInEditText(DateEditText.endDateEditText, now, new Date());
 
-        startDateEditText.setText(now);
-        endDateEditText.setText(now);
+        // Set onClickListeners for both pickers to open date pickers.
+        ((EditText) findViewById(R.id.start_date_editText)).setOnClickListener(getDateOnClickListener(START_DATE_TAG));
+        ((EditText) findViewById(R.id.end_date_editText)).setOnClickListener(getDateOnClickListener(END_DATE_TAG));
+    }
+
+    private View.OnClickListener getDateOnClickListener(final String tag) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fragment = new DatePickerDialogFragment();
+                fragment.show(getSupportFragmentManager(), tag);
+            }
+        };
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        Date datePickerDate = getDateFromDatePicker(datePicker);
+        String date = format.format(datePickerDate);
+
+        if(START_DATE_TAG.equals(fragment.getTag())) {
+            setDateInEditText(DateEditText.startDateEditText, date, datePickerDate);
+
+        } else if (END_DATE_TAG.equals(fragment.getTag())) {
+            setDateInEditText(DateEditText.endDateEditText, date, datePickerDate);
+        }
+
+        updateUI();
+    }
+
+    private boolean areCorrectDates() {
+        return (
+                startDate != null
+                && endDate != null
+                && startDate.getTime() <= endDate.getTime()
+        );
+    }
+
+    private void setDateInEditText(DateEditText editText, String text, Date date) {
+        EditText dateView;
+        switch(editText) {
+            case startDateEditText:
+                dateView = findViewById(R.id.start_date_editText);
+                dateView.setText(text);
+                startDate = date;
+                break;
+
+            case endDateEditText:
+                dateView = findViewById(R.id.end_date_editText);
+                dateView.setText(text);
+                endDate = date;
+                break;
+        }
+    }
+
+    private java.util.Date getDateFromDatePicker(DatePicker datePicker){
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year =  datePicker.getYear();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        return calendar.getTime();
+    }
+
+    private void updateUI() {
+        int color;
+
+        if(!areCorrectDates()) {
+            color = Color.RED;
+
+        } else {
+            color = Color.BLACK;
+        }
+
+        EditText startDateEditText = findViewById(R.id.start_date_editText);
+        startDateEditText.setTextColor(color);
+    }
+
+
+    public static class DatePickerDialogFragment extends android.support.v4.app.DialogFragment {
+
+        DatePickerDialog.OnDateSetListener listener;
+
+        @Override
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            listener = (DatePickerDialog.OnDateSetListener) context;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            Date date = getDate();
+            final Calendar c = Calendar.getInstance();
+            c.setTime(date);
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), listener, year, month, day);
+        }
+
+        private Date getDate() {
+
+            if(START_DATE_TAG.equals(getTag())) {
+                return startDate;
+
+            } else if (END_DATE_TAG.equals(getTag())) {
+                return endDate;
+
+            }
+
+            return new Date();
+        }
     }
 }
